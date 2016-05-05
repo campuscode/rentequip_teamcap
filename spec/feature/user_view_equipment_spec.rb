5e3d4f2eb008e7cccbe7ef1d9cc0a8067ca3710e
Matheus e Diego <matheusholv@gmail.com>
2016-05-05 20:03:56 -0300

    user view equipment - finished

--- spec/feature/user_view_equipment_spec.rb
+++ spec/feature/user_view_equipment_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
-feature 'user view equipment' do
+feature 'user view equipment list' do
 
   scenario 'successfully' do
     equipment = Equipment.create(name: 'Furadeira',
                                  price: 'R$200,00')
 
+    equipment2 = Equipment.create(name: 'Betoneira',
+                                 price: 'R$2000,00')
+
     visit equipment_index_path
 
     expect(page).to have_content (equipment.name)
     expect(page).to have_content (equipment.price)
+    expect(page).to have_content (equipment2.name)
+    expect(page).to have_content (equipment2.price)
   end
 
   scenario 'user view equipment detail' do
@@ -21,5 +26,12 @@ feature 'user view equipment' do
 
     expect(page).to have_content (equipment.name)
     expect(page).to have_content (equipment.price)
+  end
+
+  scenario 'user do not see equipment' do
+
+    visit equipment_index_path
+
+    expect(page).to have_content('Não há equipamentos cadastrados')
   end
 end
